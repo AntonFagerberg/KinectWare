ad4563f411ae94fb7fc0f0a2a7a96f539108c183
Anton Fagerberg <anton@antonfagerberg.com>
2014-04-30 11:35:37 +0200

    Marble added

---
+++ Assets/MonkeyBall/MonkeyBallWorld.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyBallWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Rotate(Vector3.forward);
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Rotate(Vector3.back);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Rotate(Vector3.left);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate(Vector3.right);
+		}
+	}
+}
---
+++ Assets/MonkeyBall/SuperMonkeyBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperMonkeyBall : MonoBehaviour {
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			rigidbody.AddTorque(Vector3.forward * speed);
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			rigidbody.AddTorque(Vector3.back * speed);
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rigidbody.AddTorque(Vector3.left * speed);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody.AddTorque(Vector3.right * speed);
+		}
+	}
+}
